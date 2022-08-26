69cdcd47eed6829fab587ec7477a906ff10f3bfc
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-08-26 16:03:51 +0200

    add contents into home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,23 @@ import Container from 'react-bootstrap/Container'
 const HomePage = () => {
 	return (
 		<Container className="py-3">
-			<h1>Welcome!</h1>
+			<div>
+				<hr />
+				<h4>Popular</h4>
+				<div>movies are here...</div>
+			</div>
+
+			<div>
+				<hr />
+				<h4>Now Playing</h4>
+				<div>movies are here...</div>
+			</div>
+
+			<div>
+				<hr />
+				<h4>Top Rated</h4>
+				<div>movies are here...</div>
+			</div>
 		</Container>
 	)
 }
